4eafe6151959a2199396b0d0e1fcd06ed195d0d8
9yubean2 <dlrbqls3024@swu.ac.kr>
2022-08-29 17:31:58 +0900

    spring option handeling

--- core/src/main/java/hello/core/AutoAppConfig.java
+++ core/src/main/java/hello/core/AutoAppConfig.java
@@ -16,11 +16,11 @@ import org.springframework.context.annotation.FilterType;
 
 public class AutoAppConfig {
 
-
   //Overriding bean definition for bean 'memoryMemberRepository' with a different definition:
  //Consider renaming one of the beans or enabling overriding by setting spring.main.allow-bean-definition-overriding=true
 // @Bean(name = "memoryMemberRepository")
 //  public MemberRepository memberRepository() {
 //   return new MemoryMemberRepository();
 //  }
+
 }
